1793d52fff4e6a71eb6e7832179c3f50b6503e8a
Katherine Atwell <katherineatwell@Katherines-MacBook-Pro.local>
2020-04-01 16:20:07 -0400

    Add the beginnings of Java object files to be interfaced w/Optaplanner

---
+++ Offering.java
@@ -0,0 +1,59 @@
+
+@PlanningEntity
+public class Offering {
+
+  private int sectionNumber;
+  private String instructorName;
+  private Course course;
+  private Room room;
+  private TimeSlot timeslot;
+
+  public Offering(int sectionNumber, String instructorName, Room room,
+    Course course, TimeSlot timeSlot) {
+      this.sectionNumber = sectionNumber;
+      this.instructorName = instructorName;
+      this.room = room;
+      this.course = course;
+      this.timeSlot = timeSlot;
+    }
+
+  public int getSectionNumber() {
+    return sectionNumber;
+  }
+
+  public void setSectionNumber(int sectionNumber) {
+    this.sectionNumber = sectionNumber;
+  }
+
+  public String getInstructorName() {
+    return instructorName;
+  }
+
+  public void setInstructorName(int getInstructorName) {
+    this.instructorName = instructorName;
+  }
+
+  public Course getCourse() {
+    return course;
+  }
+
+  public void setCourse(String Course) {
+    this.course = course;
+  }
+
+  public Room getRoom() {
+    return room;
+  }
+
+  public void setRoom(String Room) {
+    this.room = room;
+  }
+
+  public TimeSlot getTimeSlot() {
+    return timeSlot;
+  }
+
+  public void setTimeSlot(String timeSlot) {
+    this.timeSlot = timeSlot;
+  }
+}
---
+++ Parser.java
@@ -0,0 +1,14 @@
+
+
+public class Parser {
+
+  private String inputFileName;
+
+  Parser(String inputFileName) {
+    this.inputFileName = inputFileName;
+  }
+
+  public void ParseFile() {
+
+  }
+}
