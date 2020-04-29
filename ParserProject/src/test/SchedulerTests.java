9f0d812c31f435cebde29a6fd6f1b19fd6988fac
katherineatwell <katherineatwell@192.168.1.6>
2020-04-29 16:18:25 -0400

    Added top level black box tests for scheduler

--- ParserProject/src/test/SchedulerTests.java
+++ ParserProject/src/test/SchedulerTests.java
@@ -1,9 +1,14 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,12 +24,11 @@ import scheduler.TimeSlot;
 public class SchedulerTests {
 	
 	static Parser csvParser = new Parser();
-	static List<Map<String, String>> parsedCourseFile;
-	static List<Room> rooms;
-	static List<Course> courses;
-	static List<TimeSlot> times;
-	static List<Offering> offerings;
-	private File output;
+	static List<Map<String, String>> parsedSchedule;
+	private List<Room> rooms;
+	private List<Course> courses;
+	private List<TimeSlot> times;
+	private List<Offering> offerings;
 	
 	@BeforeAll 
 	public void createOutputFile() throws IOException{
@@ -34,32 +38,94 @@ public class SchedulerTests {
 		 	
 		// Parse file
 		csvParser = new Parser();
-		parsedCourseFile = csvParser.ParseCSV(courseFileName);
-		rooms = csvParser.GetRoomObjects(roomsFileName);
-		courses =  csvParser.GetCourseObjects(parsedCourseFile);
-		times = csvParser.GetTimeSlotObjects(parsedCourseFile);
-		offerings = csvParser.GetOfferingObjects(parsedCourseFile, courses, times);
+		List<Map<String, String>> parsedCourseFile = csvParser.ParseCSV(courseFileName);
+		List<Room> inputRooms = csvParser.GetRoomObjects(roomsFileName);
+		List<Course> inputCourses =  csvParser.GetCourseObjects(parsedCourseFile);
+		List<TimeSlot> inputTimes = csvParser.GetTimeSlotObjects(parsedCourseFile);
+		List<Offering> inputOfferings = csvParser.GetOfferingObjects(parsedCourseFile, courses, times);
 		
 		// Run solver and output solution
 		CourseSchedulerApp scheduler = new CourseSchedulerApp(rooms, courses, times, offerings);
 		scheduler.generateSolution();
-		scheduler.Output("schedule.csv");
+		this.rooms = scheduler.getSchedule().getRoomList();
+		this.courses = scheduler.getSchedule().getCourseList();
+		this.times = scheduler.getSchedule().getTimeSlotList();
+		this.offerings = scheduler.getSchedule().getOfferingList();
+	}
+	
+	@Test
+	public void TestOfferingsListNotEmpty() {
+		assertNotEquals(offerings.size(), 0);
+	}
+	
+	@Test
+	public void TestRoomListEqualsInputRoomList() {
 		
-		output = new File("schedule.csv");
 	}
 	
 	@Test
 	public void TestsOfferingCapacityIsNotGreaterThanRoomCapacity() {
 		
+		boolean noRoomOverCapacity = true;
+		
+        for(Offering offering : offerings) {
+        	if (offering.getCapacity() != 0 && offering.getRoom() != null && offering.getRoom().getCapacity() != 0) {
+        		if (offering.getCapacity() > offering.getRoom().getCapacity()) {
+        			noRoomOverCapacity = false; //arbitrary for now but should have higher weight than room one
+        		}
+        	}
+        }
+        
+        assertTrue(noRoomOverCapacity);
 	}
 	
 	@Test
 	public void TestsNoTeacherIsAssignedToMultipleClassesAtTheSameTime() {
 		
+		boolean noTeacherAssignedConcurrentClasses = true;
+		
+		Set<String> instructorTimes = new HashSet<>();
+        for(Offering offering : offerings) {
+        	if (offering.getTimeSlot() != null) {
+        		String instructorTimeSlot = offering.getInstructorName() +
+    				offering.getTimeSlot().getDays() +
+    				offering.getTimeSlot().getTime();
+        	
+        		if (offering.getInstructorName().compareTo("Staff") != 0) { //if instructor is not Staff
+        			if (instructorTimes.contains(instructorTimeSlot)) {
+        				noTeacherAssignedConcurrentClasses = false;
+        			}
+        			else {
+        				instructorTimes.add(instructorTimeSlot);
+        			}
+        		}
+        	}
+        }
+        
+        assertTrue(noTeacherAssignedConcurrentClasses);
 	}
 	
 	@Test
 	public void TestsNoRoomIsAssignedToMultipleClassseAtTheSameTime() {
 		
+		boolean noRoomAssignedToConcurrentClasses = true;
+		
+		Set<String> occupiedRooms = new HashSet<>();
+        for(Offering offering : offerings) {
+        	if (offering.getRoom() != null && offering.getTimeSlot() != null) {
+        		//System.out.print("enters this statement");
+        		String roomInUse = offering.getRoom().getBuilding() +
+        				offering.getRoom().getNumber() +
+        				offering.getTimeSlot().getDays() +
+        				offering.getTimeSlot().getTime();
+        		if(occupiedRooms.contains(roomInUse)){
+        			noRoomAssignedToConcurrentClasses = false;
+        		} else {
+        			occupiedRooms.add(roomInUse);
+        		}
+        	}
+        }
+        
+        assertTrue(noRoomAssignedToConcurrentClasses);
 	}
 }
